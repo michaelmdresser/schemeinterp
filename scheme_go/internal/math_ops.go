05e44dc64e6989093e6046266c8ddb0938e943fb
Michael Dresser <michaelmdresser@gmail.com>
2019-08-07 13:48:24 -0600

    cons, better concept of null, begin

--- scheme_go/internal/math_ops.go
+++ scheme_go/internal/math_ops.go
@@ -7,45 +7,74 @@ import (
 
 type Procedure = func(args ...interface{}) (interface{}, error)
 
+var Null []interface{} = []interface{}{}
+
+func isNull(i []interface{}) bool {
+	return len(i) == 0
+}
+
+// I'm not going to support pairs for now
+func Cons(args ...interface{}) (interface{}, error) {
+	if len(args) != 2 {
+		return Null, fmt.Errorf("cons requires exactly 2 arguments")
+	}
+
+	// forcing second arg to be a list or nil
+	switch a := args[1].(type) {
+	case []interface{}:
+		if isNull(a) {
+			return []interface{}{args[0]}, nil
+		}
+		return append([]interface{}{args[0]}, a...), nil
+	default:
+		return Null, fmt.Errorf("I don't support cons with a non-list (or nil) as the second arg")
+	}
+}
+
 func IsEmpty(args ...interface{}) (interface{}, error) {
 	if len(args) != 1 {
-		return nil, fmt.Errorf("empty? requires exactly 1 argument")
+		return Null, fmt.Errorf("empty? requires exactly 1 argument")
 	}
 
 	switch a := args[0].(type) {
 	case []interface{}:
-		return len(a) == 0, nil
+		return isNull(a), nil
 	default:
-		return nil, fmt.Errorf("empty? must be called on a list")
+		return Null, fmt.Errorf("empty? must be called on a list")
 	}
 }
 
 func Car(args ...interface{}) (interface{}, error) {
 	if len(args) != 1 {
-		return nil, fmt.Errorf("car requires exactly 1 argument")
+		return Null, fmt.Errorf("car requires exactly 1 argument")
 	}
 
 	switch a := args[0].(type) {
 	case []interface{}:
-		if len(a) == 0 {
-			return nil, fmt.Errorf("cannot use car on empty list")
+		if isNull(a) {
+			return Null, fmt.Errorf("cannot use car on empty list")
 		}
 		return a[0], nil
 	default:
-		return nil, fmt.Errorf("cannot call car on non-list")
+		return Null, fmt.Errorf("cannot call car on non-list")
 	}
 }
 
 func Cdr(args ...interface{}) (interface{}, error) {
 	if len(args) != 1 {
-		return nil, fmt.Errorf("cdr requires exactly 1 argument")
+		return Null, fmt.Errorf("cdr requires exactly 1 argument")
 	}
 
 	switch a := args[0].(type) {
 	case []interface{}:
-		if len(a) == 0 {
-			return nil, fmt.Errorf("cannot use cdr on empty list")
+		if isNull(a) {
+			return Null, fmt.Errorf("cannot use cdr on empty list")
 		}
+
+		//		if a[1:] == Null {
+		//			return Null, nil
+		//		}
+
 		return a[1:], nil
 	default:
 		return nil, fmt.Errorf("cannot call cdr on non-list")
--- scheme_go/main.go
+++ scheme_go/main.go
@@ -29,6 +29,23 @@ type environment struct {
 	env       map[string]interface{}
 }
 
+var baseEnv environment = environment{
+	parentEnv: nil,
+	env: map[string]interface{}{
+		"parentEnv": nil,
+		"true":      true,
+		"false":     false,
+		"+":         internal.Plus,
+		"-":         internal.Minus,
+		"*":         internal.Mult,
+		"/":         internal.Div,
+		"car":       internal.Car,
+		"cdr":       internal.Cdr,
+		"empty?":    internal.IsEmpty,
+		"cons":      internal.Cons,
+	},
+}
+
 func (e environment) lookup(name string) (interface{}, error) {
 	if val, ok := e.env[name]; ok {
 		return val, nil
@@ -44,22 +61,6 @@ func (e environment) lookup(name string) (interface{}, error) {
 	}
 
 	return val, nil
-}
-
-var baseEnv environment = environment{
-	parentEnv: nil,
-	env: map[string]interface{}{
-		"parentEnv": nil,
-		"true":      true,
-		"false":     false,
-		"+":         internal.Plus,
-		"-":         internal.Minus,
-		"*":         internal.Mult,
-		"/":         internal.Div,
-		"car":       internal.Car,
-		"cdr":       internal.Cdr,
-		"empty?":    internal.IsEmpty,
-	},
 }
 
 func tokenize(chars string) []string {
@@ -125,9 +126,22 @@ func parse(program string) (interface{}, error) {
 func eval(expr interface{}, env environment) (interface{}, environment, error) {
 	switch a := expr.(type) {
 	case []interface{}: // procedure call or keyword
-		if a[0] == "if" { // conditional
+		if len(a) == 0 { // empty list evaluates to nil
+			return internal.Null, env, nil
+		} else if a[0] == "begin" {
+			var result interface{} = internal.Null
+			var err error
+			for _, e := range a[1:] {
+				result, env, err = eval(e, env)
+				if err != nil {
+					return internal.Null, env, fmt.Errorf("failed to evaluate subexpression %v in begin", e)
+				}
+			}
+
+			return result, env, nil
+		} else if a[0] == "if" { // conditional
 			if len(a) != 4 {
-				return nil, env, fmt.Errorf("incorrect number of arguments to if: %v", a)
+				return internal.Null, env, fmt.Errorf("incorrect number of arguments to if: %v", a)
 			}
 			test := a[1]
 			consequence := a[2]
@@ -135,7 +149,7 @@ func eval(expr interface{}, env environment) (interface{}, environment, error) {
 
 			result, env, err := eval(test, env)
 			if err != nil {
-				return nil, env, fmt.Errorf("failed to evaluate test %v: %s", test, err)
+				return internal.Null, env, fmt.Errorf("failed to evaluate test %v: %s", test, err)
 			}
 
 			switch r := result.(type) {
@@ -143,22 +157,22 @@ func eval(expr interface{}, env environment) (interface{}, environment, error) {
 				if r {
 					conseqEval, env, err := eval(consequence, env)
 					if err != nil {
-						return nil, env, fmt.Errorf("failed to evaluate consequence %v: %s", consequence, err)
+						return internal.Null, env, fmt.Errorf("failed to evaluate consequence %v: %s", consequence, err)
 					}
 					return conseqEval, env, nil
 				} else {
 					altEval, env, err := eval(alternative, env)
 					if err != nil {
-						return nil, env, fmt.Errorf("failed to evaluate alternative %v: %s", alternative, err)
+						return internal.Null, env, fmt.Errorf("failed to evaluate alternative %v: %s", alternative, err)
 					}
 					return altEval, env, nil
 				}
 			default:
-				return nil, env, fmt.Errorf("test %v did not evaluate to a bool", test)
+				return internal.Null, env, fmt.Errorf("test %v did not evaluate to a bool", test)
 			}
 		} else if a[0] == "define" { // definition
 			if len(a) != 3 {
-				return nil, env, fmt.Errorf("incorrect number of arguments to define: %v", a)
+				return internal.Null, env, fmt.Errorf("incorrect number of arguments to define: %v", a)
 			}
 
 			symbol := a[1]
@@ -168,16 +182,16 @@ func eval(expr interface{}, env environment) (interface{}, environment, error) {
 			case string:
 				result, env, err := eval(symbolExpr, env)
 				if err != nil {
-					return nil, env, fmt.Errorf("failed to evaluate symbol expression %v for symbol %s: %s", symbolExpr, symbol, err)
+					return internal.Null, env, fmt.Errorf("failed to evaluate symbol expression %v for symbol %s: %s", symbolExpr, symbol, err)
 				}
 				env.env[s] = result
-				return nil, env, nil
+				return internal.Null, env, nil
 			default:
-				return nil, env, fmt.Errorf("symbol %v in define is not a string", symbol)
+				return internal.Null, env, fmt.Errorf("symbol %v in define is not a string", symbol)
 			}
 		} else if a[0] == "set!" {
 			if len(a) != 3 {
-				return nil, env, fmt.Errorf("set! requires exactly 2 arguments")
+				return internal.Null, env, fmt.Errorf("set! requires exactly 2 arguments")
 			}
 
 			symbol := a[1]
@@ -187,28 +201,28 @@ func eval(expr interface{}, env environment) (interface{}, environment, error) {
 			case string:
 				_, err := env.lookup(s)
 				if err != nil {
-					return nil, env, fmt.Errorf("set! is trying to set symbol %s which does not exist in the environment", s)
+					return internal.Null, env, fmt.Errorf("set! is trying to set symbol %s which does not exist in the environment", s)
 				}
 
 				result, env, err := eval(symbolExpr, env)
 				if err != nil {
-					return nil, env, fmt.Errorf("failed to evaluate symbol expression %v for symbol %s: %s", symbolExpr, symbol, err)
+					return internal.Null, env, fmt.Errorf("failed to evaluate symbol expression %v for symbol %s: %s", symbolExpr, symbol, err)
 				}
 				env.env[s] = result
-				return nil, env, nil
+				return internal.Null, env, nil
 			default:
-				return nil, env, fmt.Errorf("symbol %v in define is not a string", symbol)
+				return internal.Null, env, fmt.Errorf("symbol %v in define is not a string", symbol)
 			}
 
 		} else if a[0] == "quote" {
 			if len(a) != 2 {
-				return nil, env, fmt.Errorf("incorrect number of arguments to quote: %v", a)
+				return internal.Null, env, fmt.Errorf("incorrect number of arguments to quote: %v", a)
 			}
 
 			return a[1], env, nil
 		} else if a[0] == "lambda" {
 			if len(a) != 3 {
-				return nil, env, fmt.Errorf("incorrect number of arguments to lambda: %v", a)
+				return internal.Null, env, fmt.Errorf("incorrect number of arguments to lambda: %v", a)
 			}
 
 			argList := a[1]
@@ -222,11 +236,11 @@ func eval(expr interface{}, env environment) (interface{}, environment, error) {
 					case string:
 						argListString = append(argListString, a)
 					default:
-						return nil, env, fmt.Errorf("argument %v was not a string", arg)
+						return internal.Null, env, fmt.Errorf("argument %v was not a string", arg)
 					}
 				}
 			default:
-				return nil, env, fmt.Errorf("first argument to lambda %v was not a list", a)
+				return internal.Null, env, fmt.Errorf("first argument to lambda %v was not a list", a)
 			}
 
 			return closure{
@@ -237,13 +251,13 @@ func eval(expr interface{}, env environment) (interface{}, environment, error) {
 		} else { // procedure call
 			procedure, env, err := eval(a[0], env)
 			if err != nil {
-				return nil, env, fmt.Errorf("could not evaluate %v: %s", a[0], err)
+				return internal.Null, env, fmt.Errorf("could not evaluate %v: %s", a[0], err)
 			}
 			var arguments []interface{}
 			for _, unevaluatedArg := range a[1:] {
 				evaluatedArg, env, err := eval(unevaluatedArg, env)
 				if err != nil {
-					return nil, env, fmt.Errorf("could not evaluate %v: %s", unevaluatedArg, err)
+					return internal.Null, env, fmt.Errorf("could not evaluate %v: %s", unevaluatedArg, err)
 				}
 				arguments = append(arguments, evaluatedArg)
 			}
@@ -252,13 +266,13 @@ func eval(expr interface{}, env environment) (interface{}, environment, error) {
 			case internal.Procedure: // builtin
 				ret, procedureErr := p(arguments...)
 				if procedureErr != nil {
-					return nil, env, fmt.Errorf("procedure with identifier %s called with arguments %v failed: %s", a[0], arguments, procedureErr)
+					return internal.Null, env, fmt.Errorf("procedure with identifier %s called with arguments %v failed: %s", a[0], arguments, procedureErr)
 				}
 				return ret, env, nil
 			case closure:
 				evalEnv := p.env
 				if len(arguments) != len(p.argumentNames) {
-					return nil, env, fmt.Errorf("procedure expects %d arguments, got %d", len(p.argumentNames), len(arguments))
+					return internal.Null, env, fmt.Errorf("procedure expects %d arguments, got %d", len(p.argumentNames), len(arguments))
 				}
 
 				for i, arg := range arguments {
@@ -268,7 +282,7 @@ func eval(expr interface{}, env environment) (interface{}, environment, error) {
 			case []interface{}: // code as data
 				return eval(p, env)
 			case interface{}:
-				return nil, env, fmt.Errorf("procedure obtained from map with identifier %s is not a procedure", a[0])
+				return internal.Null, env, fmt.Errorf("procedure obtained from map with identifier %s is not a procedure", a[0])
 			}
 		}
 	case int64: // constant
@@ -278,11 +292,11 @@ func eval(expr interface{}, env environment) (interface{}, environment, error) {
 	case string: // variable reference
 		val, err := env.lookup(a)
 		if err != nil {
-			return nil, env, fmt.Errorf("failed to reference variable: %s", err)
+			return internal.Null, env, fmt.Errorf("failed to reference variable: %s", err)
 		}
 		return val, env, nil
 	}
-	return nil, env, fmt.Errorf("tried to evaluate the type of %v, which is not a procedure call, constant, keyword, or reference", expr)
+	return internal.Null, env, fmt.Errorf("tried to evaluate the type of %v, which is not a procedure call, constant, keyword, or reference", expr)
 }
 
 func printSliceWithTypes(s []interface{}) {
